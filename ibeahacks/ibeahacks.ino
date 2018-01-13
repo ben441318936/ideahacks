1e9a14d4953db85926c3cc2002d8a6cd0b066b83
Jingbin Huang <ben441318936@users.noreply.github.com>
2018-01-13 13:22:43 -0800

    Able to get 12hr forecast

--- ibeahacks/ibeahacks.ino
+++ ibeahacks/ibeahacks.ino
@@ -1,42 +1,30 @@
-/*
+//#include <Adafruit_HTU21DF.h>
+//#include <Wire.h>
 
-Weather Audio Notifier
-
-Hardware Required:
-* Arduino Zero Board  
-* Arduino Wifi101 Sheild
-* Piezo
-
-Software Required:
-* ArduinoJson Library
-
- created Sept 2015
- by Helena Bisby <support@arduino.cc>
-
-This example code is in the public domain
-
-http://arduino.cc/en/Tutorial/WeatherAudioNotifier
- 
-*/
-
+#include <Time.h>
+#include <TimeLib.h>
 
 #include <SPI.h>
 #include <WiFi101.h>
 #include <ArduinoJson.h>
 
+
+
 #define JSON_BUFF_DIMENSION 2500
-//#include "arduino_secrets.h" 
+//#include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = "AndroidAP";        // your network SSID (name)
 char pass[] = "Ben441318936";    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
-String nameOfCity = "Irvine,CA";   // your city of interest here in format "city,countrycode"
 String apiKey = "8e930b59cb64efd628888e8fd438cef0";
 
 String text;
 int endResponse = 0;
 boolean startJson = false;
+
+boolean timeToWater = false;
+boolean willBeRain = false;
 
 int status = WL_IDLE_STATUS;
 
@@ -46,9 +34,14 @@ WiFiClient client;
 unsigned long lastConnectionTime = 10 * 60 * 1000;     // last time you connected to the server, in milliseconds
 const unsigned long postingInterval = 10 * 60 * 1000;  // posting interval of 10 minutes  (10L * 1000L; 10 seconds delay for testing)
 
+//Adafruit_HTU21DF htu = Adafruit_HTU21DF();
+//float sensTemp = 0;
+//float sensHum = 0;
+
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
+  Serial.println("Hello world");
 
   text.reserve(JSON_BUFF_DIMENSION);
 
@@ -70,9 +63,14 @@ void setup() {
   }
   // you're connected now, so print out the status:
   printWifiStatus();
+
+  /*if (!htu.begin()) {
+    Serial.println("Couldn't find sensor!");
+    while (1);
+  }*/
 }
 
-boolean stopplz=false;
+boolean stopplz = false;
 
 void loop() {
 
@@ -85,38 +83,52 @@ void loop() {
     httpRequest();
   }
 
-  if (!client.available() && !stopplz){
+  /*if (!client.available() && !stopplz){
     Serial.println("No incoming data");
-  }
-  while (client.available()) {
+    }
+    while (client.available()) {
     stopplz = true;
     char c = client.read();
     Serial.write(c);
-  }
+    }*/
 
-  //char c = 0;
-  /*if (client.available()) {
+  char c = 0;
+  if (client.available()) {
+    stopplz = true;
     c = client.read();
     // json contains equal number of open and close curly brackets, therefore by counting
     // the open and close occurences, we can determine when a json is completely received
-    
-    // endResponse == 0 means equal number of open and close curly brackets reached 
-    if (endResponse == 0 && startJson == true) {
-      parseJson(text.c_str());  // parse c string text in parseJson function
-      text = "";                // clear text string for the next time
-      startJson = false;        // set startJson to false to indicate that a new message has not yet started
-    }
+
     if (c == '{') {
       startJson = true;         // set startJson true to indicate json message has started
       endResponse++;
+      Serial.println("Open curly");
     }
     if (c == '}') {
       endResponse--;
+      Serial.println("Close curly");
     }
     if (startJson == true) {
       text += c;
     }
-  }*/
+    Serial.println(endResponse);
+  }
+  else {
+    if (!stopplz)
+      Serial.println("No incoming data");
+  }
+  // endResponse == 0 means equal number of open and close curly brackets reached
+  if (endResponse == 0 && startJson == true) {
+    Serial.println("Done counting braces, parsing now...");
+    parseJson(text.c_str());  // parse c string text in parseJson function
+    text = "";                // clear text string for the next time
+    startJson = false;        // set startJson to false to indicate that a new message has not yet started
+  }
+
+  //sensTemp = htu.readTemperature();
+  //sensHum = htu.readHumidity();
+
+  
 }
 void parseJson(const char * jsonString) {
   StaticJsonBuffer<4000> jsonBuffer;
@@ -129,26 +141,89 @@ void parseJson(const char * jsonString) {
   }
 
   JsonArray& list = root["list"];
-  JsonObject& now = list[0];
-  JsonObject& later = list[1];
+  JsonObject& hour3 = list[0];
+  JsonObject& hour6 = list[1];
+  JsonObject& hour9 = list[2];
+  JsonObject& hour12 = list[3];
 
   String city = root["city"]["name"];
-  float tempNow = now["main"]["temp"];
-  float humidityNow = now["main"]["humidity"];
-  String weatherNow = now["weather"][0]["description"];
 
-  float tempLater = later["main"]["temp"];
-  float humidityLater = later["main"]["humidity"];
-  String weatherLater = later["weather"][0]["description"];
+  String timeHour3 = hour3["dt_txt"];
+  float tempHour3 = hour3["main"]["temp"];
+  float humidityHour3 = hour3["main"]["humidity"];
+  String weatherHour3 = hour3["weather"][0]["description"];
 
-  printDiffFloat(tempNow, tempLater, "temperature", "*C");
-  printDiffString(weatherNow, weatherLater, "rain");
-  printDiffString(weatherNow, weatherLater, "snow");
-  printDiffString(weatherNow, weatherLater, "hail");
-  printDiffString(weatherNow, weatherLater, "clear");
-  printDiffFloat(humidityNow, humidityLater, "humidity", "%");
+  String timeHour6 = hour6["dt_txt"];
+  float tempHour6 = hour6["main"]["temp"];
+  float humidityHour6 = hour6["main"]["humidity"];
+  String weatherHour6 = hour6["weather"][0]["description"];
+
+  String timeHour9 = hour9["dt_txt"];
+  float tempHour9 = hour9["main"]["temp"];
+  float humidityHour9 = hour9["main"]["humidity"];
+  String weatherHour9 = hour9["weather"][0]["description"];
+
+  String timeHour12 = hour12["dt_txt"];
+  float tempHour12 = hour12["main"]["temp"];
+  float humidityHour12 = hour12["main"]["humidity"];
+  String weatherHour12 = hour12["weather"][0]["description"];
+
+  if (weatherHour3 == "Rain" || weatherHour6 == "Rain" || weatherHour9 == "Rain" || weatherHour12 == "Rain") {
+    willBeRain = true;
+  }
+
+  printWeather(timeHour3,tempHour3,humidityHour3,weatherHour3,"*C");
+  printWeather(timeHour6,tempHour6,humidityHour6,weatherHour6,"*C");
+  printWeather(timeHour9,tempHour9,humidityHour9,weatherHour9,"*C");
+  printWeather(timeHour12,tempHour12,humidityHour12,weatherHour12,"*C");
+
   Serial.println();
 
+}
+
+void printWeather(String dt_txt, float temp, float humidity, String weather, String unit){
+  Serial.println(dt_txt);
+  Serial.println("Temperature: " + String(temp) + unit);
+  Serial.println("Humidity: " + String(humidity) + "%");
+  Serial.println("Weather: " + weather);
+  Serial.println("");
+}
+
+void isTimeToWater(float currHum, boolean willRain){
+  if (!willRain) {
+    if (currHum < 50) {
+      timeToWater = true;
+    }
+  }
+}
+
+void printTime(float dt) {
+  dt = dt;
+  int hr = hour(dt);
+  int mi = minute(dt);
+  int da = day(dt);
+  int mon = month(dt);
+  int yr = year(dt);
+
+  String monString = "";
+
+  switch(mon){
+    case 1: monString = "January";
+    case 2: monString = "Febuary";
+    case 3: monString = "March";
+    case 4: monString = "April";
+    case 5: monString = "May";
+    case 6: monString = "June";
+    case 7: monString = "July";
+    case 8: monString = "August";
+    case 9: monString = "September";
+    case 10: monString = "October";
+    case 11: monString = "November";
+    case 12: monString = "December";
+    default: monString = "January";
+  }
+
+  Serial.println(String(hr)+":"+String(mi)+" "+ monString +" " + String(da)+", " + String(yr));
 }
 
 // this method makes a HTTP connection to the server:
@@ -157,48 +232,22 @@ void httpRequest() {
   // This will free the socket on the WiFi shield
   client.stop();
 
-  Serial.println("\nStarting connection to server..."); 
- // if you get a connection, report back via serial: 
- if (client.connect(server, 80)) { 
-   Serial.println("connected to server"); 
-   // Make a HTTP request: 
-   client.print("GET /data/2.5/group?"); 
-   client.print("id=5368361"); 
-   client.print("&appid="+apiKey); 
-   client.print("&cnt=3"); 
-   client.println("&units=metric HTTP/1.1"); 
-   client.println("Host: api.openweathermap.org");   
-   //client.println("Connection: close"); 
-   client.println(); 
- } else { 
-   Serial.println("unable to connect"); 
- } 
-}
-
-void printDiffString(String now, String later, String weatherType) {
-  int indexNow = now.indexOf(weatherType);
-  int indexLater = later.indexOf(weatherType);
-  // for all types of weather except for clear skies, if the current weather does not contain the weather type and the later message does, send notification
-  Serial.println("Weather now: " + String(indexNow));
-  Serial.println("Weather later: " + String(indexLater));
-}
-
-void printDiffFloat(float now, float later, String parameter, String unit) {
-  String change;
-  if (now > later) {
-    change = "drop from ";
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.print("GET /data/2.5/forecast?");
+    client.print("id=5368361");
+    client.print("&appid=" + apiKey);
+    client.print("&cnt=4"); // data is for every 3 hours, cnt=4 gives 12-hour look ahead
+    client.println("&units=metric HTTP/1.1");
+    client.println("Host: api.openweathermap.org");
+    //client.println("Connection: close");
+    client.println();
+  } else {
+    Serial.println("unable to connect");
   }
-  else if (now < later) {
-    change = "rise from ";
-  }
-  else {
-    return;
-  }
-  Serial.print("UPDATE: The " + parameter + " will " + change);
-  Serial.print(now);
-  Serial.print(unit + " to ");
-  Serial.print(later);
-  Serial.println(unit + "!");
 }
 
 void printWifiStatus() {
